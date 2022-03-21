56743eb726b106e56d1807873ce26ee76145c1cc
etomedina <hmedina@LAPTOP-84M4TVEB.localdomain>
2022-03-20 20:43:15 -0400

    Primer commit correspondiente a Random Card Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let NumberSuit = ["♦", "♥", "♠", "♣"];
+  let NumberCard = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+
+  let random1 = Math.floor(Math.random() * NumberSuit.length);
+  let random2 = Math.floor(Math.random() * NumberCard.length);
+
+  let pinta1 = NumberSuit[random1];
+  let valor = NumberCard[random2];
+  let pinta2 = pinta1;
+
+  console.log(pinta1);
+  console.log(valor);
+
+  document.querySelector("#suit1").innerHTML = pinta1;
+  document.querySelector("#card").innerHTML = valor;
+  document.querySelector("#suit2").innerHTML = pinta2;
 };
